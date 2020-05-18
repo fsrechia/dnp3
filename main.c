96718b7fdae001bbaf04fd8ff916829b41a7102b
Felipe Rechia <feliperechia@gmail.com>
2020-05-18 15:41:20 -0300

    Initial commit
    - Add sample code received
    - Add README template with assignment instructions
    
    Signed-off-by: Felipe Rechia <feliperechia@gmail.com>

---
+++ main.c
@@ -0,0 +1,41 @@
+/**************************************************************************
+This function illustrates how to compute the CRC and check it for a
+received data block.
+**************************************************************************/
+
+int main() {
+    short idx; // Index
+    unsigned char dataBlock[18]; // Array to hold received data block
+    short blockSize; // Size of data block, not including CRC octets
+    unsigned short crc; // 16-bit check code (crc accumulator)
+    
+    // ...
+    // Insert code here to receive contents of dataBlock and blockSize;
+    
+    // ...
+    // Compute check code for data in received block
+
+    crc = 0; // Initialize
+    for (idx = 0; idx < blockSize; idx++)
+    computeCRC(dataBlock[idx],&crc);
+    crc = ~crc; // Invert
+    
+    // Check CRC at end of block
+    if (dataBlock[idx++] == (unsigned char)crc &&
+    dataBlock[idx] == (unsigned char)(crc >> 8))
+    {
+        // Block received without error
+        // ...
+        // Insert code here to process good block
+        // ...
+    }
+    else
+    {
+        // Error discovered
+        // ...
+        // Insert error processing code here
+        // ...
+    }
+    // ...
+    return 0;
+}
